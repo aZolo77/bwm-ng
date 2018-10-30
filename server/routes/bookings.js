0307f68ee9774e0320cbb9eac283a5dca70facf1
aZolo77 <a.burtsev@hostland.ru>
2018-10-30 16:59:51 +0300

    Adds manage rental and bookings endpoints and delete rental endpoint

--- server/routes/bookings.js
+++ server/routes/bookings.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const UserCtrl = require('../controllers/user');
 const BookingCtrl = require('../controllers/booking');
 
+// = creating new booking
 router.post('', UserCtrl.authMiddlewear, BookingCtrl.createBooking);
+// = managing bookings of this User
+router.get('/manage', UserCtrl.authMiddlewear, BookingCtrl.getUserBookings);
 
 module.exports = router;
