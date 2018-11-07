1c1178d4dc915473bfaa7ffa41339263e6bea004
aZolo77 <a.burtsev@hostland.ru>
2018-11-07 11:57:03 +0300

    temporary changes on image upload

---
+++ server/services/image-upload.js
@@ -0,0 +1,38 @@
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const config = require('../config');
+
+aws.config.update({
+  accessKeyId: config.AWS_SECRET_ACCESS_KEY,
+  secretAccessKey: config.AWS_ACESS_KEY_ID,
+  region: 'us-east-2'
+});
+
+const s3 = new aws.S3();
+
+// checking file mimetype
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(new Error('Invalid file type, only JPEG and PNG are allowed'), false);
+  }
+};
+
+const upload = multer({
+  fileFilter,
+  storage: multerS3({
+    acl: 'public-read',
+    s3,
+    bucket: 'bwm-zolo-ng',
+    metadata: function(req, file, cb) {
+      cb(null, { fieldName: 'TESTING_METADATA' });
+    },
+    key: function(req, file, cb) {
+      cb(null, Date.now().toString());
+    }
+  })
+});
+
+module.exports = upload;
